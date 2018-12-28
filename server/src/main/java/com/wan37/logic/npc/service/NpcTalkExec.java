b7072309b15d16f178244171dfcc9747f93d2c0a
SterbenDa <270801364@qq.com>
2018-12-28 17:10:56 +0800

    Bug：npc对话没抛出事件

--- server/src/main/java/com/wan37/logic/npc/service/NpcTalkExec.java
+++ server/src/main/java/com/wan37/logic/npc/service/NpcTalkExec.java
@@ -1,5 +1,7 @@
 package com.wan37.logic.npc.service;
 
+import com.wan37.event.GenernalEventListenersManager;
+import com.wan37.event.entity.NpcTalkEvent;
 import com.wan37.exception.GeneralErrorExecption;
 import com.wan37.logic.mission.MissionCanAcceptListGetter;
 import com.wan37.logic.mission.config.MissionCfg;
@@ -27,9 +29,15 @@ public class NpcTalkExec {
     @Autowired
     private MissionEncoder missionEncoder;
 
+    @Autowired
+    private GenernalEventListenersManager genernalEventListenersManager;
+
     public void exec(Player player, Integer npcId) {
         NpcCfg npcCfg = npcCfgLoader.load(npcId)
                 .orElseThrow(() -> new GeneralErrorExecption("找不到对应Npc配置表"));
+
+        // 抛出NPC对话事件
+        genernalEventListenersManager.fireEvent(new NpcTalkEvent(npcId, player));
 
         //TODO: 检查场景npc
 
